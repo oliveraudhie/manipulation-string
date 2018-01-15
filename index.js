3c9be81dda7b2335649f96a34279eec7c12e1510
oliveraudhie <slumantri@gmail.com>
2018-01-15 14:00:13 +0700

    level 2

--- index.js
+++ index.js
@@ -10,12 +10,3 @@ const d = `${a} ${b}`
 console.log(a.toUppercase());
 console.log(b.toUppercase());
 // level 3
-function titleCase(str) {
-  str = str.toLowerCase().split(' ')
-  for (var i = 0; i < str.length; i++) {
-    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1)
-  }
-  return str.join(' ')
-}
-
-console.log(titleCase("haLoHelo"))
